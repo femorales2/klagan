0c3cad0e3bfd01c30e67256364ff13b22f822aa9
Felix Morales <felixmorales0392@gmail.com>
2024-04-28 19:02:05 -0300

    favorite label

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,17 @@ import SearchInput from '@/components/SearchInput/SearchInput';
 import styles from '@/styles/Home.module.scss';
 import classNames from 'classnames';
 import CardsWrapper from '@/components/CardsWrapper/CardsWrapper';
+import { useCardContext } from '@/context/CardContext';
 
 export default function Home() {
+  const {enabledFavoriteView} = useCardContext();
   return (
     <div className={classNames(styles.home)}>
+      {
+        enabledFavoriteView && (
+          <h2>Favorites</h2>
+        )
+      }
       <SearchInput />
       <CardsWrapper />
     </div>
